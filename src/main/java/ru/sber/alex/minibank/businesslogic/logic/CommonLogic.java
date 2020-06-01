ff9aceb29ed94f4618278c9bb531df756a9fa7d0
AlexPit <a;expit63@gmail.com>
2020-06-01 22:33:05 +0300

    new credentials for AWS

--- src/main/java/ru/sber/alex/minibank/businesslogic/logic/CommonLogic.java
+++ src/main/java/ru/sber/alex/minibank/businesslogic/logic/CommonLogic.java
@@ -13,26 +13,29 @@ public class CommonLogic {
 
     /**
      * Запрашивает у Security логин текущего пользователя, выполняющего запрос.
+     *
      * @return String логин пользователя
      */
-    public static String getCurrentLogin(){
+    public static String getCurrentLogin() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName();
     }
 
     /**
      * Проверяет сумму на 0 или отрицательное значение. Если оно таковое - заполняет модель необходимыми данными
-     * @param sum Сумма транзакции
+     *
+     * @param sum   Сумма транзакции
      * @param model контейнер переменных страницы
      * @return true - есть ошибка, false - нет ошибки
      */
-    public static boolean checkSum(BigDecimal sum, Model model){
+    public static boolean checkSum(BigDecimal sum, Model model) {
         if (sum.doubleValue() <= 0.00) {
             String error = "Сумма должна быть больше 0!";
             model.addAttribute("userError", true);
             model.addAttribute("errorMessage", error);
             return true;
-        } else
+        } else {
             return false;
+        }
     }
 }
